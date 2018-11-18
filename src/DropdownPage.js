fc94a8ec7b2a5d0bd4c0843be3ed7f5a9ab95173
baurine <2008.hbl@gmail.com>
2018-11-18 21:16:01 +0800

    Primitive dropdown component

---
+++ src/DropdownPage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './DropdownPage.css';
+
+class DropdownPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dropdownVisible: false
+    }
+  }
+
+  toggleDropdown = (e) => {
+    this.setState(prevState => ({dropdownVisible: !prevState.dropdownVisible}))
+  }
+
+  renderDropdownMenu() {
+    return (
+      <div className='dropdown-body'>
+        <div>
+          <input type='checkbox'/><span>option 1</span>
+        </div>
+        <div>
+          <input type='checkbox'/><span>option 2</span>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className='dropdown-container'>
+        <div className='dropdown-trigger'>
+          <button onClick={this.toggleDropdown}>
+            dropdown trigger
+          </button>
+        </div>
+        {
+          this.state.dropdownVisible &&
+          this.renderDropdownMenu()
+        }
+      </div>
+    )
+  }
+}
+
+export default DropdownPage;
